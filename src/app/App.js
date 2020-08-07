60d7a5b6eb994d08f69621f9b2b406c60d3945da
Alex Thomas <hello@cawfree.com>
2020-08-07 11:19:06 +0100

    chore: verify basic apple pay transactions

--- src/app/App.js
+++ src/app/App.js
@@ -4,94 +4,40 @@ import withPaymentRequest from "react-payment-request-api";
 
 import {MaybePaymentButton} from "./components";
 
+// XXX: Obviously this is super apple-pay specific, but we can extend this in future.
 const App = ({isServerSide, methodData, details, options, ...extraProps}) => {
+  const [applyPayAvailable] = useState(
+    () => (window.ApplePaySession && ApplePaySession.canMakePayments() && window.PaymentRequest),
+  );
   const onClick = useCallback(
     () => {
-      if (window.ApplePaySession && ApplePaySession.canMakePayments()) {
-        console.log('supported');
-        var request = null;
-        if (window.PaymentRequest) {
-          console.log('found payment request obj');
-          // TODO: should be methods
-          request = new PaymentRequest(methodData, details, options);
-          console.log('made request');
-
-          console.log('setting validation');
-          request.onmerchantvalidation = function (event) {
-            console.log('got validation');
-            console.log('event is', event);
-            console.log(event.validationURL);
-            // TODO: what the hell is this part?
-            const sessionPromise = Promise.resolve(event.validationURL);
-            console.log('about to complete');
-            event.complete(sessionPromise);
-          };
-          
-          request.show()
-            .then(console.log)
-            .catch(console.error);
-          //const status = processResponse(response);
-          //response.complete(status);
-        } else {
-          console.log('consider apple pay js instead');
-        // Consider using Apple Pay JS instead.
-        }
-      } else {
-        console.log('not supported');
+      if () {
+        const request = new PaymentRequest(methodData, details, options);
+        request.onmerchantvalidation = (event) => {
+          console.log('event is', event);
+          console.log(event.validationURL);
+          // TODO: Manage internal negotation of the payment? Use express _somehow_.
+          //       How to actually let it through.
+          const sessionPromise = Promise.resolve(event.validationURL);
+          // XXX: TODO: Completion state should be defined by the api.
+          return new Promise(resolve => setTimeout(resolve, 2000))
+            .then(() => event.complete("success"));
+        };
+        return request.show();
       }
+      return Promise.reject(new Error(`Sorry, payment is not supported.`));
     },
   );
-  return (
-    <div
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: "green",
-      }}
-    >
+  if (applePayAvailable) {
+    // TODO: Use a nice graphic!
+    return (
       <button
-        onClick={onClick}
-        children="Pay"
+        onClick={() => onClick().then(console.log).catch(console.error)}
+        children="Pay with ApplePay"
       />
-    </div>
-  );
-  //const [Button] = useState(
-  //  () => !isServerSide ? withPaymentRequest()(MaybePaymentButton) : MaybePaymentButton
-  //);
-  //const onShowSuccess = useCallback(
-  //  (result, resolve, reject) => {
-  //    console.log('Result:', result);
-  //    resolve(result);
-  //    alert(result);
-  //  },
-  //);
-  //const onShowFail = useCallback(
-  //  (error) => {
-  //    console.error(error);
-  //    alert(error);
-  //  },
-  //);
-  //const onMerchantValidation = useCallback(
-  //  (event) => {
-  //    console.log(event);
-  //    event.complete(Promise.resolve(event.validationURL));
-  //  },
-  //);
-  //return (
-  //  <Button
-  //    config={{
-  //      methodData,
-  //      details,
-  //      options,
-  //      onShowSuccess,
-  //      onShowFail,
-  //      onMerchantValidation,
-  //      /* ignored */
-  //      onShippingAddressChange: (request, resolve, reject) => resolve(details),
-  //      onShippingOptionChange: (request, resolve, reject) => resolve(details), 
-  //    }}
-  //  />
-  //);
+    );
+  }
+  return "Sorry, Apple Pay is not supported.";
 };
 
 App.displayName = "App";
