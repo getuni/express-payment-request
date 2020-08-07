33e22ca7f3c841c5059266c83da537cf42c8df9a
Alex Thomas <hello@cawfree.com>
2020-08-07 10:30:56 +0100

    0.0.1-alpha.2

--- src/app/App.js
+++ src/app/App.js
@@ -5,43 +5,76 @@ import withPaymentRequest from "react-payment-request-api";
 import {MaybePaymentButton} from "./components";
 
 const App = ({isServerSide, methodData, details, options, ...extraProps}) => {
-  const [Button] = useState(
-    () => !isServerSide ? withPaymentRequest()(MaybePaymentButton) : MaybePaymentButton
-  );
-  const onShowSuccess = useCallback(
-    (result, resolve, reject) => {
-      console.log('Result:', result);
-      resolve(result);
-      alert(result);
-    },
-  );
-  const onShowFail = useCallback(
-    (error) => {
-      console.error(error);
-      alert(error);
-    },
-  );
-  const onMerchantValidation = useCallback(
-    (event) => {
-      console.log(event);
-      event.complete(Promise.resolve(event.validationURL));
+  const onClick = useCallback(
+    () => {
+      if (window.ApplePaySession && ApplePaySession.canMakePayments()) {
+        console.log('supported');
+        var request = null;
+        if (window.PaymentRequest) {
+          console.log('found payment request obj');
+          // TODO: should be methods
+          request = new PaymentRequest(methodData, details, options);
+          console.log('made request');
+
+          console.log('setting validation');
+          request.onmerchantvalidation = function (event) {
+            console.log('got validation');
+            console.log('event is', event);
+            console.log(event.validationURL);
+            // TODO: what the hell is this part?
+            const sessionPromise = Promise.resolve(event.validationURL);
+            console.log('about to complete');
+            event.complete(sessionPromise);
+          };
+        } else {
+          console.log('consider apple pay js instead');
+        // Consider using Apple Pay JS instead.
+        }
+      } else {
+        console.log('not supported');
+      }
     },
   );
   return (
-    <Button
-      config={{
-        methodData,
-        details,
-        options,
-        onShowSuccess,
-        onShowFail,
-        onMerchantValidation,
-        /* ignored */
-        onShippingAddressChange: (request, resolve, reject) => resolve(details),
-        onShippingOptionChange: (request, resolve, reject) => resolve(details), 
-      }}
-    />
+    <button style="-webkit-appearance: -apple-pay-button;" onClick={onClick} />
   );
+  //const [Button] = useState(
+  //  () => !isServerSide ? withPaymentRequest()(MaybePaymentButton) : MaybePaymentButton
+  //);
+  //const onShowSuccess = useCallback(
+  //  (result, resolve, reject) => {
+  //    console.log('Result:', result);
+  //    resolve(result);
+  //    alert(result);
+  //  },
+  //);
+  //const onShowFail = useCallback(
+  //  (error) => {
+  //    console.error(error);
+  //    alert(error);
+  //  },
+  //);
+  //const onMerchantValidation = useCallback(
+  //  (event) => {
+  //    console.log(event);
+  //    event.complete(Promise.resolve(event.validationURL));
+  //  },
+  //);
+  //return (
+  //  <Button
+  //    config={{
+  //      methodData,
+  //      details,
+  //      options,
+  //      onShowSuccess,
+  //      onShowFail,
+  //      onMerchantValidation,
+  //      /* ignored */
+  //      onShippingAddressChange: (request, resolve, reject) => resolve(details),
+  //      onShippingOptionChange: (request, resolve, reject) => resolve(details), 
+  //    }}
+  //  />
+  //);
 };
 
 App.displayName = "App";
