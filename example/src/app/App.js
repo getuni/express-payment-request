bfc5282534c1cfc78adbb7b864a77102894c7765
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-08-09 22:58:58 +0100

    chore: external result handling

--- example/src/app/App.js
+++ example/src/app/App.js
@@ -3,9 +3,31 @@ import PropTypes from "prop-types";
 
 function App({ postMessageStream }) {
   const [details, setDetails] = useState(null);
+  const [result, setResult] = useState(null);
   useEffect(
-    () => postMessageStream.on('data', ({type, details}) => (type === "details") && setDetails(details)) && undefined,
+    () => postMessageStream.on(
+      "data",
+      ({type, ...extras}) => {
+        if (type === "details") {
+          const {details} = extras;
+          return setDetails(details);
+        } else if (type === "result") {
+          const {result} = extras;
+          return setResult(result);
+        }
+        return console.warn(`Encountered unexpected type, "${type}".`);
+      },
+    ),
     [postMessageStream, setDetails],
+  );
+  useEffect(
+    () => {
+      if (!!result) {
+        return postMessageStream.write({type: "resolve"}) && undefined;
+      }
+      return undefined;
+    },
+    [postMessageStream, result],
   );
   return (
     <div>
@@ -13,7 +35,7 @@ function App({ postMessageStream }) {
         <div children={JSON.stringify(details)} />
       )}
       <button
-        onClick={() => postMessageStream.write({ type: "request-payment" })}
+        onClick={() => postMessageStream.write({type: "request-payment"})}
         style={{
           backgroundColor: "pink",
         }}
--- src/app/App.js
+++ src/app/App.js
@@ -5,38 +5,70 @@ import {encode as btoa} from "base-64";
 
 // XXX: Obviously this is super apple-pay specific, but we can extend this in future.
 const App = ({isServerSide, methodData, details, options, path, host, postMessageStream, ...extraProps}) => {
+  const [latch] = useState([{
+    resolve: null, reject: null, result: null,
+  }]);
+
+  const shouldResolve = useCallback(() => latch[0].resolve(latch[0].result), [latch]);
+  const shouldReject = useCallback(() => latch[0].reject(new Error("Implementor rejected payment completion.")), [latch]);
 
   const requestPayment = useCallback(
     () => {
       if (!window.PaymentRequest) {
         return Promise.reject(new Error(`Payment Requests are not supported on this browser.`));
       }
+      
+      latch[0].result = null;
 
       const request = new PaymentRequest(methodData, details, options);
+
       request.onmerchantvalidation = (event) => {
         const {validationURL: url} = event;
         return event
           .complete(
-            axios({ url: `${host}${path}/validate?url=${btoa(url)}`, method: 'get' })
-              .then(({data}) => data),
+            new Promise(
+              (resolve, reject) => {
+                latch[0].resolve = resolve;
+                latch[0].reject = reject;
+                return axios({url: `${host}${path}/validate?url=${btoa(url)}`, method: "get"})
+                  .then(({data}) => data)
+                  .then(
+                    (result) => {
+                      latch[0].result = result;
+                      return postMessageStream.write({type: "result", result});
+                    },
+                  );
+              },
+            ),
             false,
           );
       };
-
       return request.show()
         .then(response => response.complete("success"));
     },
-    [methodData, details, options],
+    [postMessageStream, methodData, details, options, latch],
   );
 
   useEffect(
-    () => postMessageStream.write({ type: "details", details }) && undefined,
+    () => postMessageStream.write({type: "details", details}) && undefined,
     [postMessageStream, details],
   );
 
   useEffect(
-    () => postMessageStream.on("data", ({type}) => (type === "request-payment") && requestPayment()) && undefined,
-    [postMessageStream, requestPayment],
+    () => postMessageStream.on(
+      "data",
+      ({type, ...extras}) => {
+        if (type === "request-payment") {
+          return requestPayment();
+        } else if (type === "resolve") {
+          return shouldResolve();
+        } else if (type === "reject") {
+          return shouldReject();
+        }
+        return console.warn(`Encountered unexpected type, "${type}". This will be ignored.`);
+      },
+    ) && undefined,
+    [postMessageStream, requestPayment, shouldResolve, shouldReject],
   );
 
   return null;
